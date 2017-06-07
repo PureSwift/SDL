aef18beaf4b0b78719ca1989f42cb3c5a1b16e7f
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 11:15:01 -0500

    renamed Demo target to SDLDemo
    to avoid name collision

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             name: "SDL",
             targets: ["SDL"]),
         .executable(
-            name: "Demo",
-            targets: ["Demo"]),
+            name: "SDLDemo",
+            targets: ["SDLDemo"]),
     ],
     dependencies: [
         .package(url: "https://github.com/PureSwift/CSDL2.git", .branch("master"))
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Demo",
+            name: "SDLDemo",
             dependencies: ["SDL"]),
         .target(
             name: "SDL",
