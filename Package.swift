5a11cd431968be795cfeb67ab0b2e751ab70187d
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-10-19 02:03:14 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,20 +1,32 @@
-// swift-tools-version:3.0.2
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "SDL",
-    targets: [
-                 Target(
-                    name: "SDLDemo",
-                    dependencies: [.Target(name: "SDL")]
-					),
-                 Target(
-                    name: "SDL"
-					)
-    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SDL",
+            targets: ["SDL"]),
+        .executable(
+            name: "SDLDemo",
+            targets: ["SDLDemo"]),
+        ],
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CSDL2.git", majorVersion: 1)
-    ]
+        .package(url: "https://github.com/PureSwift/CSDL2.git", .branch("master"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SDLDemo",
+            dependencies: ["SDL"]),
+        .target(
+            name: "SDL",
+            dependencies: []),
+        .testTarget(
+            name: "SDLTests",
+            dependencies: ["SDL"]),
+        ]
 )
