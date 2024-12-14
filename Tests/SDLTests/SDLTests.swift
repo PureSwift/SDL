50eb3aa33c87020a54ce7a92055f1d1f0cf52634
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-14 15:31:56 -0500

    Updated unit tests

--- Tests/SDLTests/SDLTests.swift
+++ Tests/SDLTests/SDLTests.swift
@@ -1,10 +1,36 @@
+import Foundation
 import XCTest
 @testable import SDL
 
 class SDLTests: XCTestCase {
     
-    func testExample() {
+    func testDrivers() {
         
-        
+        print("Available Render Drivers:")
+        let renderDrivers = SDLRenderer.Driver.all
+        XCTAssert(renderDrivers.count > 0)
+        if renderDrivers.isEmpty == false {
+            print("=======")
+            for driver in renderDrivers {
+                
+                do {
+                    let info = try SDLRenderer.Info(driver: driver)
+                    print("Driver:", driver.rawValue)
+                    print("Name:", info.name)
+                    print("Options:")
+                    info.options.forEach { print("  \($0)") }
+                    print("Formats:")
+                    info.formats.forEach { print("  \($0)") }
+                    if info.maximumSize.width > 0 || info.maximumSize.height > 0 {
+                        print("Maximum Size:")
+                        print("  Width: \(info.maximumSize.width)")
+                        print("  Height: \(info.maximumSize.height)")
+                    }
+                    print("=======")
+                } catch {
+                    print("Could not get information for driver \(driver.rawValue)")
+                }
+            }
+        }
     }
 }
