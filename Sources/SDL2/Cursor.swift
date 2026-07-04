71fbed7b97adc5c470f6198e3bc43c4a4b8cc6d1
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-03 22:49:53 -0400

    Add `SDLCursor`

---
+++ Sources/SDL2/Cursor.swift
@@ -0,0 +1,67 @@
+//
+//  Cursor.swift
+//  SDL2
+//
+//  Created by Alsey Coleman Miller on 6/6/17.
+//
+
+import CSDL2
+
+/// SDL Cursor
+public final class SDLCursor {
+
+    // MARK: - Properties
+
+    internal let internalPointer: OpaquePointer
+
+    internal let isOwner: Bool
+
+    // MARK: - Initialization
+
+    deinit {
+        if isOwner {
+            SDL_FreeCursor(internalPointer)
+        }
+    }
+
+    internal init(_ internalPointer: OpaquePointer, isOwner: Bool = true) {
+        self.internalPointer = internalPointer
+        self.isOwner = isOwner
+    }
+
+    /// Create a color cursor from a surface.
+    public init(surface: SDLSurface, hotspot: (x: Int, y: Int)) throws(SDLError) {
+
+        let internalPointer = SDL_CreateColorCursor(surface.internalPointer, Int32(hotspot.x), Int32(hotspot.y))
+        self.internalPointer = try internalPointer.sdlThrow(type: "SDLCursor")
+        self.isOwner = true
+    }
+
+    // MARK: - Accessors
+
+    /// The default cursor.
+    ///
+    /// - Note: This cursor is owned by SDL and should not be destroyed.
+    public static var `default`: SDLCursor {
+        // force unwrap: guaranteed non-null by SDL, always available
+        SDLCursor(SDL_GetDefaultCursor()!, isOwner: false)
+    }
+}
+
+// MARK: - SDL
+
+public extension SDL {
+
+    /// Set the active cursor.
+    ///
+    /// - Parameter cursor: The cursor to show, or `nil` to restore the default cursor.
+    static func setCursor(_ cursor: SDLCursor?) {
+        SDL_SetCursor(cursor?.internalPointer ?? SDL_GetDefaultCursor())
+    }
+
+    /// Whether the cursor is currently shown.
+    static var isCursorVisible: Bool {
+        get { SDL_ShowCursor(SDL_QUERY) == SDL_ENABLE }
+        set { SDL_ShowCursor(newValue ? SDL_ENABLE : SDL_DISABLE) }
+    }
+}
